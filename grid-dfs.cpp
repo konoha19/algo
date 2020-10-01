fd93eb9992559bd85cb999a863979f6310364e6c
Ravi Sable <ravi19299@gmail.com>
2020-10-01 14:19:56 +0530

    cut-edge

--- grid-dfs.cpp
+++ grid-dfs.cpp
@@ -1,62 +1,47 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 //std::vector<std::vector<int> > ar;
 char ar[1001][1001];
 bool vis[1001][1001];
 int n,m;
-
-
 bool isValid(int x,int y)
 {
-	if(x<1 || x>n || y<1 || y>m)
-		return false;
-	if(vis[x][y]==true || ar[x][y]=='#')
-		return false;
-	return true;
+ if(x<1 || x>n || y<1 || y>m)
+  return false;
+ if(vis[x][y]==true || ar[x][y]=='#')
+  return false;
+ return true;
 }
-
 void dfs(int x,int y)
 {
-	
-	vis[x][y]=true;
-	if(isValid(x-1,y))
-		dfs(x-1,y);
-
-	if(isValid(x+1,y))
-		dfs(x+1,y);
-
-	if(isValid(x,y-1))
-		dfs(x,y-1);
-
-	if(isValid(x,y+1))
-		dfs(x,y+1);
-
+ vis[x][y]=true;
+ if(isValid(x-1,y))
+  dfs(x-1,y);
+ if(isValid(x+1,y))
+  dfs(x+1,y);
+ if(isValid(x,y-1))
+  dfs(x,y-1);
+ if(isValid(x,y+1))
+  dfs(x,y+1);
 }
-
-
 int main()
 {
-  	ios_base::sync_with_stdio(false); cin.tie(NULL);
+   ios_base::sync_with_stdio(false); cin.tie(NULL);
     cin>>n>>m;
-
     for(int i=1;i<=n;i++)
     {
-    	for(int j=1;j<=m;j++)
-    		cin>>ar[i][j];
+     for(int j=1;j<=m;j++)
+      cin>>ar[i][j];
     }
-
-
-	int cc=0;
-	for(int i=1;i<=n;i++)
-	{
-		for(int j=1;j<=m;j++)
-			if(ar[i][j]=='.' && vis[i][j]==false  )
-			{
-				dfs(i,j);
-				cc++;
-			}
-	}
-	cout<<cc<<"\n";
-
+ int cc=0;
+ for(int i=1;i<=n;i++)
+ {
+  for(int j=1;j<=m;j++)
+   if(ar[i][j]=='.' && vis[i][j]==false )
+   {
+    dfs(i,j);
+    cc++;
+   }
+ }
+ cout<<cc<<"\n";
 }
